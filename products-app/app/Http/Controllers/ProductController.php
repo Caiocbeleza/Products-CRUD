0fa7421b1573bb63a69d9131a556338f75b33181
Caio César Beleza <caio.almeidabeleza@gmail.com>
2023-07-04 19:47:35 -0300

    Implementing method Create

--- products-app/app/Http/Controllers/ProductController.php
+++ products-app/app/Http/Controllers/ProductController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 use App\Models\Product;
+use App\Http\Requests\ProductStoreRequest;
+
 
 class ProductController extends Controller
 {
@@ -32,9 +36,29 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ProductStoreRequest $request)
     {
-        //
+        try{
+            $imageName = Str::random(32).".".$request->image->getClientOriginalExtension();
+
+            //Create Product
+            Product::create([
+                'name' => $request->name,
+                'image' => $imageName,
+                'description' => $request->description
+            ]);
+
+            //Save image in Storage folder
+            Storage::disk('public')->put($imageName, file_get_contents($request->image));
+
+            return response()->json([
+                'message' => "Product successfully created!"
+            ], 200);
+        } catch(\Exception $e){
+            return response()->json([
+                'message' => "Something went wrong!"
+            ], 500);
+        }
     }
 
     /**
---
+++ products-app/app/Http/Requests/ProductStoreRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        if(request()->isMethod('post')){
+            return [
+                'name' => 'required|string|max:258',
+                'image' => 'required|image|mimes:jpeg,png,jpg,gif,sgv|max:2048',
+                'description' => 'required|string'
+            ];
+        } else {
+            return [
+                'name' => 'required|string|max:258',
+                'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,sgv|max:2048',
+                'description' => 'required|string'
+            ];
+        }
+    }
+
+    public function messages(){
+        if(request()->isMethod('post')){
+            return [
+                'name.required' => 'Name is Required!',
+                'image.required' => 'image is Required!',
+                'description.required' => 'description is Required!'
+            ];
+        } else {
+            return [
+                'name.required' => 'Name is Required!',
+                'description.required' => 'description is Required!'
+            ];
+        }
+
+    }
+}

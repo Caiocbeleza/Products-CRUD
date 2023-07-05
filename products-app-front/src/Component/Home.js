f529997e69fb97612fb23f89397899b2497b3c1a
Caio César Beleza <caio.almeidabeleza@gmail.com>
2023-07-05 10:17:51 -0300

    Starting frontend

---
+++ products-app-front/src/Component/Home.js
@@ -0,0 +1,16 @@
+import React, { } from "react";
+
+function Home(){
+    return(
+        <React.Fragment>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1>React Laravel CRUD</h1>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+export default Home;
